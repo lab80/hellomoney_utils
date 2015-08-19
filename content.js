f899c912afdc30aad4ad9f864323d38151b2db75
Woo Jin <woog2nius@gmail.com>
2015-08-19 14:16:10 +0900

    Background highlight feature added

--- content.js
+++ content.js
@@ -1,5 +1,16 @@
-$('li.item').each(function() {
-  if(!$(this).children('.js-holding-details').length) {
-    $(this).click();
-  }
-});
+$("li.item:not(:has('.js-holding-details'))").click();
+
+if(window.__interval) clearInterval(window.__interval);
+window.__interval = setInterval(function() {
+  $(".metric:not(:has('.pill.active'))").css({
+    'background-color': 'beige'
+  });
+  $(".metric:has('.pill.active')").css({
+    'background-color': 'transparent'
+  });
+}, 1000);
+
+setTimeout(function() {
+  clearInterval(window.__interval);
+  delete window.__interval;
+}, 10000);
